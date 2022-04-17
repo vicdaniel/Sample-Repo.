6bfff1d696a42ccb2b05c9753314e2e244d0e30e
Vic Daniel <research0845@rogers.com>
2022-04-16 23:45:51 -0400

    Modified some code

--- script.py
+++ script.py
@@ -1,16 +1,17 @@
-import sys
+# import sys
 import requests
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+# def greet(who_to_greet):
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
 
 
 # print(greet('World'))
 # print(greet('Victor'))
 r = requests.get("https://microsoft.com")
 print(r.status_code)
+print(r.ok)
