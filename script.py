170522cb2a0702a7f53ba492f7dc80051bbb0ac8
Vic Daniel <research0845@rogers.com>
2022-04-16 21:30:52 -0400

    Initial commit

---
+++ script.py
@@ -0,0 +1,16 @@
+import sys
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+# print(greet('World'))
+# print(greet('Victor'))
+r = requests.get("https://microsoft.com")
+print(r.status_code)
